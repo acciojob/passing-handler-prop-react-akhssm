c82f04fe7529f1ebed4dd0803cd2d12d8c4a3920
Akshay <mandaakshay20@gmail.com>
2025-11-26 10:36:50 +0530

    Wrote code According to the given Condition

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
+import React, { useState } from "react";
 
+const Selection = ({ applyColor }) => {
+  const [selectionStyle, updateSelectionStyle] = useState({
+    background: "",
+  });
+
+  return (
+    <div
+      className="fix-box"
+      style={selectionStyle}
+      onClick={() => applyColor(updateSelectionStyle)}
+    >
+      <h4>Selection</h4>
+    </div>
+  );
+};
+
+export default Selection;
